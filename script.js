646d6de275d67032aeddafc897f0fbb78c7750fd
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-09-13 18:29:33 +0300

    Added IDs for links and finished mobile menu

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const burgerIcon = document.querySelector("header .links img")
 const mobileMenu = document.querySelector(".mobile-menu")
 const closeIcon = document.querySelector(".close-icon img")
 
@@ -6,9 +5,11 @@ document.body.addEventListener("click", (e) => {
   if (e.target.matches("header .links img")) {
     mobileMenu.classList.toggle("invisible")
     return
-  }
-  if (e.target.matches(".close-icon img") || e.target.matches('.mobile-menu .links a')) {
-    console.log('closed menu')
+  } else if (
+    e.target.matches(".close-icon img") ||
+    e.target.matches(".mobile-menu .links a")
+  ) {
+    console.log("closed menu")
     mobileMenu.classList.toggle("invisible")
     return
   }
